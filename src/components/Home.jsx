7ddf172c9f9748f22e65512db9c148c0da3ac5cc
leeskies <lee2dskies@gmail.com>
2023-03-19 20:27:48 +0200

    yo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import * as hi2 from 'react-icons/hi2'
 
 export const Home = () => {
+
+  // states
+  const [movies, setMovies] = useState([])
+
+  // functoins
+  const fetchMovies = async () => {
+    const { data } = await axios.get('https://www.omdbapi.com/?s=heist&apikey=cfcc70f2')
+    setMovies(data.Search)
+  }
+
+  useEffect(() => {
+    fetchMovies()
+  },[])
+
   return (
-    <div>Home</div>
+    <div>
+      <section className='grid grid-cols-4 gap-4 justify-items-center mx-auto my-0 container'>
+        {movies.map((movie, i) => (
+          <article key={i} className='bg-gray-500'>
+            <button className='relative bg-slate-200 w-full h-full hidden opacity-0 hover:flex hover:opacity-100'><hi2.HiPlay /></button>
+            <img src={movie.Poster} className='w-full' alt="" />
+            <div></div>
+          </article>
+        ))}
+      </section>
+    </div>
   )
 }
--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
+import '../header.css'
 
 export const Header = () => {
   return (
-    <nav className='navbar flex justify-end bg-gray-600'>
-        <figure className='px-2'><NavLink className={'btn'} to={'/'}>Home</NavLink></figure>
-        <figure className='px-2'><NavLink className={'btn'} to={'/cars'}>Cars</NavLink></figure>
-        {/* <div className='dropdown dropdown-hover dropdown-end'>
-        <label tabIndex={0} className="btn m-1">Hover</label>
-            <ul tabIndex={0} className={"dropdown-content menu p-2 shadow bg-gray-400 rounded-box w-52"}>
-                <li><a>Item 1</a></li>
-                <li><a>Item 2</a></li>
-            </ul>
-        </div> */}
-    </nav>
+    <div>
+      <header className='w-full shadow-xl bg-gray-200 header-logo text-6xl text-gray-900 p-4'>VOODOO</header>
+      <Outlet />
+    </div>
   )
 }
