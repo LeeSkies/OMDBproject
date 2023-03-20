7aaab8b8431bf63bf6898be8dc18428d35cfcf19
leeskies <lee2dskies@gmail.com>
2023-03-21 00:15:55 +0200

    yo

---
+++ src/components/Slide.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Slider from ''
+
+export const Slide = () => {
+
+  const images = [
+    'https://img.zoechip.com/resize/1278x768/aa/33/aa339e48d99bfc4c962ea95dbe5f0fa7/aa339e48d99bfc4c962ea95dbe5f0fa7.jpg',
+    'https://img.zoechip.com/resize/1278x768/f6/23/f623deaea2107717d129a52cd6f72252/f623deaea2107717d129a52cd6f72252.jpg',
+    'https://img.zoechip.com/resize/1278x768/64/54/6454697be5467e53604ec0887b733a7d/6454697be5467e53604ec0887b733a7d.jpg',
+  ]
+
+  return (
+    <div>Slide</div>
+  )
+}
