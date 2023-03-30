6f61777ea8260c86d3fa5c81a99c3c4ae8d9aae6
leeskies <lee2dskies@gmail.com>
2023-03-30 20:08:25 +0300

    yo

---
+++ index.js
@@ -0,0 +1,11 @@
+// const { age } = object
+
+// const returnObject = () => {
+//     const object = {
+//         name: "shalom",
+//         age: "25"
+//     }
+//     return object
+// }
+
+// console.log(returnObject().name)
